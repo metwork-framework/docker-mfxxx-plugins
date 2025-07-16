5c47fb2fbb370013ba068fabc4c64ecba92eabad
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-07-16 12:46:24 +0000

    build: fix docker push

--- .github/workflows/docker_build.sh
+++ .github/workflows/docker_build.sh
@@ -6,7 +6,7 @@ set -x
 docker pull $IMAGE 2>/dev/null
 HASH_BEFORE=`docker images -q $IMAGE` 
 docker build -t $IMAGE --build-arg BRANCH=$BRANCH --build-arg REPO=$REPO --build-arg ROCKY=$ROCKY --build-arg ROCKY_NUMBER=$ROCKY_NUMBER --build-arg CACHEBUST=$CACHEBUST .
-HASH_AFTER=`docker images -q $TAG_BRANCH`
+HASH_AFTER=`docker images -q $IMAGE`
 if [ "${HASH_BEFORE}" != "${HASH_AFTER}" ]; then
-    docker push $TAG_BRANCH
+    docker push $IMAGE
 fi
