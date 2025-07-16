dd3824748c9baa44159ae2305c3de52b857e0a4d
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-07-16 13:18:30 +0000

    build: case tag starting by v

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -8,17 +8,27 @@ set -x
 
 case "${GITHUB_EVENT_NAME}" in
     repository_dispatch)
+	if [[ ${PAYLOAD_TAG} == v* ]]; then
+	  TAG=${PAYLOAD_TAG##v}
+	else
+	  TAG=${PAYLOAD_TAG}
+	fi
         BRANCH=${PAYLOAD_BRANCH}
         REPO=${PAYLOAD_REPO}
         ROCKY=rocky${PAYLOAD_ROCKY_VERSION}
         ROCKY_NUMBER=${PAYLOAD_ROCKY_VERSION}
-        IMAGE=metwork/${PAYLOAD_REPO}-rocky${PAYLOAD_ROCKY_VERSION}:${PAYLOAD_TAG};;
+        IMAGE=metwork/${PAYLOAD_REPO}-rocky${PAYLOAD_ROCKY_VERSION}:${TAG};;
     workflow_dispatch)
+	if [[ ${WORKFLOW_TAG} == v* ]]; then
+	  TAG=${WORKFLOW_TAG##v}
+	else
+	  TAG=${WORKFLOW_TAG}
+	fi
         BRANCH=${WORKFLOW_BRANCH}
         REPO=${WORKFLOW_REPO}
         ROCKY=rocky${WORKFLOW_ROCKY_VERSION}
         ROCKY_NUMBER=${WORKFLOW_ROCKY_VERSION}
-        IMAGE=metwork/${WORKFLOW_REPO}-rocky${WORKFLOW_ROCKY_VERSION}:${WORKFLOW_TAG};;
+        IMAGE=metwork/${WORKFLOW_REPO}-rocky${WORKFLOW_ROCKY_VERSION}:${TAG};;
 esac
 
 echo "branch=${BRANCH}" >> ${GITHUB_OUTPUT}
