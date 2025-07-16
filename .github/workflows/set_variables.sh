81f85fc51e3aecc4530c11adbe21dcded4adc7ef
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-07-16 12:27:20 +0000

    build: fix again

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -6,21 +6,19 @@
 #set -eu
 set -x
 
-echo ${GITHUB_EVENT_NAME}
-
 case "${GITHUB_EVENT_NAME}" in
     repository_dispatch)
-        BRANCH="${PAYLOAD_BRANCH}"
-        REPO="${PAYLOAD_REPO}"
-        ROCKY="rocky${PAYLOAD_ROCKY_VERSION}"
-        ROCKY_NUMBER="${PAYLOAD_ROCKY_VERSION}"
-        IMAGE="metwork/${PAYLOAD_REPO}-rocky${PAYLOAD_ROCKY_VERSION}:${PAYLOAD_TAG}";;
-    worflow_dispatch)
-        BRANCH="${WORKFLOW_BRANCH}"
-        REPO="${WORKFLOW_REPO}"
-        ROCKY="rocky${WORKFLOW_ROCKY_VERSION}"
-        ROCKY_NUMBER="${WORKFLOW_ROCKY_VERSION}"
-        IMAGE="metwork/${WORKFLOW_REPO}-rocky${WORKFLOW_ROCKY_VERSION}:${WORKFLOW_TAG}";;
+        BRANCH=${PAYLOAD_BRANCH}
+        REPO=${PAYLOAD_REPO}
+        ROCKY=rocky${PAYLOAD_ROCKY_VERSION}
+        ROCKY_NUMBER=${PAYLOAD_ROCKY_VERSION}
+        IMAGE=metwork/${PAYLOAD_REPO}-rocky${PAYLOAD_ROCKY_VERSION}:${PAYLOAD_TAG};;
+    workflow_dispatch)
+        BRANCH=${WORKFLOW_BRANCH}
+        REPO=${WORKFLOW_REPO}
+        ROCKY=rocky${WORKFLOW_ROCKY_VERSION}
+        ROCKY_NUMBER=${WORKFLOW_ROCKY_VERSION}
+        IMAGE=metwork/${WORKFLOW_REPO}-rocky${WORKFLOW_ROCKY_VERSION}:${WORKFLOW_TAG};;
 esac
 
 echo "branch=${BRANCH}" >> ${GITHUB_OUTPUT}
